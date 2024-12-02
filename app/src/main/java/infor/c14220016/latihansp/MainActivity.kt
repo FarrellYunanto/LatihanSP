e2a2d1d6174e6c83c6fae498c4ea7694cc6e2d00
FarrellYunanto <farrellyunanto@gmail.com>
2024-12-03 00:55:39 +0700

    nampilkan data dudmmy

--- app/src/main/java/infor/c14220016/latihansp/MainActivity.kt
+++ app/src/main/java/infor/c14220016/latihansp/MainActivity.kt
@@ -1,12 +1,34 @@
 package infor.c14220016.latihansp
 
+import android.content.DialogInterface
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import java.util.Date
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var _nama: MutableList<String> = emptyList<String>().toMutableList()
+    private var _tanggal: MutableList<Date> = emptyList<Date>().toMutableList()
+    private var _kategori: MutableList<String> = emptyList<String>().toMutableList()
+    private var _deskripsi: MutableList<String> = emptyList<String>().toMutableList()
+    private var _status: MutableList<String> = emptyList<String>().toMutableList()
+
+    lateinit var sp: SharedPreferences
+    private lateinit var _rvTask: RecyclerView
+    private var arTask= arrayListOf<task>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +38,97 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        _rvTask = findViewById<RecyclerView>(R.id.rvTasks)
+        sp = getSharedPreferences("dataSP", MODE_PRIVATE)
+        val gson = Gson()
+        val isiSP = sp.getString("spTask", null)
+        val type = object : TypeToken<ArrayList<task>>() {}.type
+        if(isiSP != null){
+            arTask = gson.fromJson(isiSP, type)
+        }
+
+        if (arTask.size == 0){
+            siapkanData()
+        } else {
+            arTask.forEach{
+                _nama.add(it.nama)
+                _tanggal.add(it.tanggal)
+                _kategori.add(it.kategori)
+                _deskripsi.add(it.deskripsi)
+                _status.add(it.status)
+            }
+            arTask.clear()
+        }
+
+        tambahData()
+        tampilkanData()
+    }
+
+    fun siapkanData(){
+        _nama = mutableListOf("Name 1", "Name 2", "Name 3")
+        _tanggal = mutableListOf(
+            Date(2024 - 1900, 0, 1),
+            Date(2024 - 1900, 1, 14),
+            Date(2024 - 1900, 2, 21)
+        )
+        _kategori = mutableListOf("Kat 1", "Kat 2", "Kat 3")
+        _deskripsi = mutableListOf("Des 1", "Des 2", "Des 3")
+        _status = mutableListOf("Stat 1", "Stat 2", "Stat 3")
+    }
+
+    fun tambahData(){
+        val gson = Gson()
+        val editor = sp.edit()
+        arTask.clear()
+        for (position: Int in _nama.indices){
+            val data = task(
+                _nama[position],
+                _tanggal[position],
+                _kategori[position],
+                _deskripsi[position],
+                _status[position]
+            )
+            arTask.add(data)
+        }
+        val json = gson.toJson(arTask)
+        editor.putString("spTask", json)
+        editor.apply()
+    }
+
+    fun tampilkanData(){
+        _rvTask.layoutManager = StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL)
+        val adapterTask = adapterRecView(arTask)
+        _rvTask.adapter = adapterTask
+
+        adapterTask.setOnItemClickCallback(object: adapterRecView.OnItemClickCallback{
+            override fun delData(pos: Int) {
+                AlertDialog.Builder(this@MainActivity)
+                    .setTitle("HAPUS DATA")
+                    .setMessage("Apakah Benar Data " + _nama[pos]+" akan dihapus ?")
+                    .setPositiveButton(
+                        "HAPUS",
+                        DialogInterface.OnClickListener { dialog, which ->
+                            _nama.removeAt(pos)
+                            _tanggal.removeAt(pos)
+                            _kategori.removeAt(pos)
+                            _deskripsi.removeAt(pos)
+                            _status.removeAt(pos)
+                            tambahData()
+                            tampilkanData()
+                        }
+                    )
+                    .setNegativeButton(
+                        "BATAL",
+                        DialogInterface.OnClickListener { dialog, which ->
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Data Batal Dihapus",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    ).show()
+            }
+        })
     }
 }
--- app/src/main/java/infor/c14220016/latihansp/adapterRecView.kt
+++ app/src/main/java/infor/c14220016/latihansp/adapterRecView.kt
@@ -1,34 +1,14 @@
 package infor.c14220016.latihansp
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import java.util.Date
 
 class adapterRecView(private val listtask: ArrayList<task>): RecyclerView
 .Adapter<adapterRecView.ListViewHolder>() {
-
-    inner class ListViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-//        var nama : String,
-//        var vartanggal : Date,
-//        var kategori : String,
-//        var deskripsi : String,
-//        var status : String,
-
-        var _namaTask = itemView.findViewById<TextView>(R.id.namaTask)
-        var _tanggalTask = itemView.findViewById<TextView>(R.id.tanggalTask)
-        var _kategoriTask = itemView.findViewById<TextView>(R.id.kategoriTask)
-        var _deskripsiTask = itemView.findViewById<TextView>(R.id.deskripsiTask)
-
-        var _hapusBtn = itemView.findViewById<Button>(R.id.delBtn)
-        var _ubahBtn = itemView.findViewById<Button>(R.id.editBtn)
-        var _kerjakanBtn = itemView.findViewById<Button>(R.id.actionBtn)
-    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -38,22 +18,6 @@ class adapterRecView(private val listtask: ArrayList<task>): RecyclerView
             .inflate(R.layout.item_recycler, parent, false)
         return ListViewHolder(view)
     }
-
-    override fun getItemCount(): Int {
-        return listtask.size
-    }
-
-    private lateinit var onItemClickCallback: OnItemClickCallback
-
-    interface OnItemClickCallback {
-        fun onItemClicked(data:task)
-        fun delData(pos:Int)
-    }
-
-    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback){
-        this.onItemClickCallback = onItemClickCallback
-    }
-
 
     override fun onBindViewHolder(holder: adapterRecView.ListViewHolder, position: Int) {
         var task: task = listtask[position]
@@ -66,5 +30,32 @@ class adapterRecView(private val listtask: ArrayList<task>): RecyclerView
         holder._hapusBtn.setOnClickListener {
             onItemClickCallback.delData(position)
         }
+    }
+
+    override fun getItemCount(): Int {
+        return listtask.size
+    }
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    interface OnItemClickCallback {
+//        fun onItemClicked(data:task)
+        fun delData(pos:Int)
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback){
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    inner class ListViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+        var _namaTask = itemView.findViewById<TextView>(R.id.namaTask)
+        var _tanggalTask = itemView.findViewById<TextView>(R.id.tanggalTask)
+        var _kategoriTask = itemView.findViewById<TextView>(R.id.kategoriTask)
+        var _deskripsiTask = itemView.findViewById<TextView>(R.id.deskripsiTask)
+
+        var _hapusBtn = itemView.findViewById<Button>(R.id.delBtn)
+        var _ubahBtn = itemView.findViewById<Button>(R.id.editBtn)
+        var _kerjakanBtn = itemView.findViewById<Button>(R.id.actionBtn)
     }
 }
